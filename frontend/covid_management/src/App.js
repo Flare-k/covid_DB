6f06e90c7cfd01555ffe4f56baf55bb84dec7e9b
Flare-k <rokkyw@naver.com>
2020-11-10 01:16:56 +0900

    Add Material UI

--- frontend/covid_management/src/App.js
+++ frontend/covid_management/src/App.js
@@ -1,6 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import Patient from './components/Patient';
+import Paper from '@material-ui/core/Paper';    //Component의 외부를 감싼다. div -> Paper
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+// style variable 지정
+const styles = theme => ({
+    root: {
+        width:'100%',
+        // marginTop: theme.spacing(3),//에러생김...
+        overflowX:"auto" //x축으로 오버플로우 발생 가능
+    },
+    table: {
+        minWidth: 1080  //Table은 1080px이상
+    }
+})
 
 const patients = [
     {
@@ -9,7 +28,7 @@ const patients = [
         'name': '신%%',
         'birthday': '940603',
         'gender': '남자',
-        'job': 'Air-Force'
+        'job': '직업군인'
     },
     {
         'id': 2,
@@ -30,26 +49,39 @@ const patients = [
 ];
 
 function App() {
-  return (
-    <div>
-        {
-            patients.map(patient => {
-                return(
-                    <Patient
-                        key = {patient.id}
-                        id = {patient.id}
-                        image = {patient.image}
-                        name = {patient.name}
-                        birthday = {patient.birthday}
-                        gender = {patient.gender}
-                        job = {patient.job}
-                    />
-                )
-            })
-        }
+    const classes  = styles();
+    return (
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>번호</TableCell>
+                        <TableCell>이미지</TableCell>
+                        <TableCell>이름</TableCell>
+                        <TableCell>생년월일</TableCell>
+                        <TableCell>성별</TableCell>
+                        <TableCell>직업</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {patients.map(patient => {
+                        return(
+                            <Patient
+                                key = {patient.id}
+                                id = {patient.id}
+                                image = {patient.image}
+                                name = {patient.name}
+                                birthday = {patient.birthday}
+                                gender = {patient.gender}
+                                job = {patient.job}
+                            />
+                        )
+                    })}
+                </TableBody>
+            </Table>
 
-    </div>
-  );
+        </Paper>
+    );
 }
 
-export default App;
+export default withStyles(styles)(App); // withStyles(styles)를 적용하여 내보낸다.
--- frontend/covid_management/src/components/Patient.js
+++ frontend/covid_management/src/components/Patient.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Patient extends React.Component{
     render(){
         return(
-            <div>
-                <PatientProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <PatientInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         );
     }
 }
+/*
 class PatientProfile extends React.Component{
     render(){
         return(
@@ -32,5 +39,5 @@ class PatientInfo extends React.Component{
         );
     }
 }
-
+*/
 export default Patient;
