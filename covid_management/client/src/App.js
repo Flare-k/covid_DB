1fc79744877d140f2fb13a5961f4cccf1bbd99fb
Flare-k <rokkyw@naver.com>
2020-11-11 16:11:44 +0900

    Connecting Client & Server

--- covid_management/client/src/App.js
+++ covid_management/client/src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Patient from './components/Patient';
 import Paper from '@material-ui/core/Paper';    //Component의 외부를 감싼다. div -> Paper
@@ -13,7 +14,7 @@ import {withStyles} from '@material-ui/core/styles';
 const styles = theme => ({
     root: {
         width:'100%',
-        // marginTop: theme.spacing(3),//에러생김...
+        marginTop: theme.spacing(3),
         overflowX:"auto" //x축으로 오버플로우 발생 가능
     },
     table: {
@@ -21,67 +22,56 @@ const styles = theme => ({
     }
 })
 
-const patients = [
-    {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/1',
-        'name': '신%%',
-        'birthday': '940603',
-        'gender': '남자',
-        'job': '직업군인'
-    },
-    {
-        'id': 2,
-        'image': 'https://placeimg.com/64/64/2',
-        'name': '강##',
-        'birthday': '940529',
-        'gender': '남자',
-        'job': '대학생'
-    },
-    {
-        'id': 3,
-        'image': 'https://placeimg.com/64/64/3',
-        'name': '조@@',
-        'birthday': '941003',
-        'gender': '남자',
-        'job': '취준생'
+class App extends Component{
+    state = {
+        patients: ""
+    }//환자 정보를 state 값으로 가져온다.
+
+    componentDidMount(){
+        this.callApi()
+            .then(res => this.setState({patients:res}))
+            .catch(err => console.log(err));
     }
-];
+    callApi = async() => {
+        const response = await fetch('/api/patients');  //비동기 통신으로 접속하고자 하는 주소를 넣는다.
+        const body = await response.json();     // 불러온 json 데이터를 body에 넣는다.
+        return body;    //컴포넌트가 마운트되면 리턴된다.
+    }
+    render(){
+        const {classes} = this.props;
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.patients ? this.state.patients.map(patient => {
+                            return(
+                                <Patient
+                                    key = {patient.id}
+                                    id = {patient.id}
+                                    image = {patient.image}
+                                    name = {patient.name}
+                                    birthday = {patient.birthday}
+                                    gender = {patient.gender}
+                                    job = {patient.job}
+                                />
+                            )
+                        }): ""}
+                    </TableBody>
+                </Table>
 
-function App() {
-    const classes  = styles();
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>번호</TableCell>
-                        <TableCell>이미지</TableCell>
-                        <TableCell>이름</TableCell>
-                        <TableCell>생년월일</TableCell>
-                        <TableCell>성별</TableCell>
-                        <TableCell>직업</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {patients.map(patient => {
-                        return(
-                            <Patient
-                                key = {patient.id}
-                                id = {patient.id}
-                                image = {patient.image}
-                                name = {patient.name}
-                                birthday = {patient.birthday}
-                                gender = {patient.gender}
-                                job = {patient.job}
-                            />
-                        )
-                    })}
-                </TableBody>
-            </Table>
-
-        </Paper>
-    );
+            </Paper>
+        );
+    }
 }
 
 export default withStyles(styles)(App); // withStyles(styles)를 적용하여 내보낸다.
--- covid_management/server.js
+++ covid_management/server.js
@@ -10,8 +10,33 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // json, html, text, urlencoded 할 거 없이 다 parser할 수 있도록 설정해줘야 한다.
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: "Hello~ Express Server"});
+app.get('/api/patients', (req,res)=>{
+    res.send([
+    {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '신%%',
+        'birthday': '940603',
+        'gender': '남자',
+        'job': '직업군인'
+    },
+    {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '강##',
+        'birthday': '940529',
+        'gender': '남자',
+        'job': '대학생'
+    },
+    {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '조@@',
+        'birthday': '941003',
+        'gender': '남자',
+        'job': '취준생'
+    }
+]);
 });
 
 const handleListening = () => {
