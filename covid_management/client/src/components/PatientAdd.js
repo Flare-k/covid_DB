b676a8e00d0bd46b0ee027ef8cc7ab1968e9d3b7
Flare-k <rokkyw@naver.com>
2020-11-15 22:53:35 +0900

    Add PatientAdd

--- covid_management/client/src/components/PatientAdd.js
+++ covid_management/client/src/components/PatientAdd.js
@@ -12,15 +12,16 @@ class PatientAdd extends React.Component{
             birthday: '',
             gender: '',
             job: '',
-            fileName: ""
+            fileName: ''
         }
     }
     handleFormSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         this.addPatient()
-            .then((res) => {
-                console.log(res.data);
-            });     // 파일을 전송하면 콘솔창에 보이도록
+        .then((res) => {
+            console.log(res.data);
+        })     // 파일을 전송하면 콘솔창에 보이도록
+        window.location.reload(); // 페이지를 바로 새로고침하여 확진자 데이터를 받아올 수 있도록 구현
     }
 
     handleFileChange = (e) => {
@@ -32,18 +33,18 @@ class PatientAdd extends React.Component{
 
     handleValueChange = (e) => {
         const nextState = {};
-        nextState[e.target.name] = e.target.value
+        nextState[e.target.name] = e.target.value;
         this.setState(nextState);
     }
 
     addPatient = () =>{
         const url = '/api/patients';
         const formData = new FormData();
-        formData.append('image', this.state.file);
-        formData.append('name', this.state.userName);
-        formData.append('birthday', this.state.birthday);
-        formData.append('gender', this.state.gender);
-        formData.append('job', this.state.job);
+        formData.append('image', this.state.file)
+        formData.append('name', this.state.userName)
+        formData.append('birthday', this.state.birthday)
+        formData.append('gender', this.state.gender)
+        formData.append('job', this.state.job)
         // 파일이 포함된 데이터를 서버로 보낼때 -> 헤더 필요
         const config = {
             headers: {
@@ -56,15 +57,15 @@ class PatientAdd extends React.Component{
 
     render() {
         return(
-            <from onSubmit={this.handleFormSubmit}>
+            <form onSubmit={this.handleFormSubmit}>
                 <h1>확진자 추가</h1>
-                병원 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input> <br/>
-                이름: <input type="text" name = "userName" value={this.state.userName} onChange={this.state.handleValueChange}></input> <br/>
-                생년월일: <input type="text" name = "birthday" value={this.state.birthday} onChange={this.state.handleValueChange}></input> <br/>
-                성별: <input type="text" name = "gender" value={this.state.gender} onChange={this.state.handleValueChange}></input> <br/>
-                직업: <input type="text" name = "job" value={this.state.job} onChange={this.state.handleValueChange}></input> <br/>
+                병원 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /> <br/>
+                이름: <input type="text" name = "userName" value={this.state.userName} onChange={this.handleValueChange} /> <br/>
+                생년월일: <input type="text" name = "birthday" value={this.state.birthday} onChange={this.handleValueChange} /> <br/>
+                성별: <input type="text" name = "gender" value={this.state.gender} onChange={this.handleValueChange} /> <br/>
+                직업: <input type="text" name = "job" value={this.state.job} onChange={this.handleValueChange} /> <br/>
                 <button type="submit">추가하기</button>
-            </from>
+            </form>
         )
     }
 }
--- covid_management/server.js
+++ covid_management/server.js
@@ -3,17 +3,21 @@ import "@babel/polyfill";
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import multer from "multer";    // 파일 전송을 위한 라이브러리
 import dbConfig from "./config/db";
 const conn = dbConfig.init();
 dbConfig.connect(conn);
-
-
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+const upload = multer({dest:'./upload'});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // json, html, text, urlencoded 할 거 없이 다 parser할 수 있도록 설정해줘야 한다.
+
+// 사용자 입장에서 image라는 이름의 경로로 접근을 하는데, 실제 서버의 upload 폴더와 매핑되어 있다.
+app.use('/image', express.static('./upload'));   // static을 이용해 upload 폴더를 공유한다. image 폴더에서 upload 폴더로 접근한다.
 
 app.get('/api/patients', async (req,res) => {
     await conn.query(
@@ -23,6 +27,23 @@ app.get('/api/patients', async (req,res) => {
         }
     );
 });
+
+app.post('/api/patients', upload.single('image'), (req, res) => {
+    const sql = "INSERT INTO PATIENT VALUES (null, ?, ?, ?, ?, ?)";
+    const image = '/image/' + req.file.filename;    // 사용자는 image 경로에 있는 해당 파일이름으로 접근한다.
+    // 즉, image라는 변수로 실제로 프로필 이미지의 바이너리 데이터를 서버에 전송 ->  그때의 파일이름도 같이 전송받는데 multer가 자동으로 겹치지 않게 설정해준다.
+    const name = req.body.name;
+    const birthday = req.body.birthday;
+    const gender = req.body.gender;
+    const job = req.body.job;
+    const params = [image, name, birthday, gender, job];
+    conn.query(sql, params, 
+        (err, rows, field) => {
+            res.send(rows);
+        }
+    );
+})
+
 
 const handleListening = () => {
   console.log(`✅  Listening on: http://localhost:${PORT}`);
